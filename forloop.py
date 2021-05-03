31f9bd57526f30a0c97a68b925e053e5c9fb3512
nandakrishnan nair <nandu.99.nair@gmail.com>
2021-05-03 23:32:46 +0530

    Initial commit

---
+++ forloop.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 23:22:03 2021
+
+@author: nandu
+"""
+
+for i in range(5):
+    print("hello")
---
+++ hellonandu.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 23:13:39 2021
+
+@author: nandu
+"""
+
+print("hello nandu sir")
+print("hello manu sir")
